7c9e60a644fa94168fdf236ac29d61df0aa60450
EN <b5903612@gmail.com>
2022-05-28 19:20:04 +0800

    v.3

--- app/src/main/java/com/example/enshootgame/MainActivity.kt
+++ app/src/main/java/com/example/enshootgame/MainActivity.kt
@@ -5,8 +5,13 @@ import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
 import com.example.enshootgame.databinding.ActivityMainBinding
 import kotlinx.coroutines.*
+
+@GlideModule
+public final class MyAppGlideModule : AppGlideModule()
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +20,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     var flag:Boolean = false
     lateinit var job: Job
+    lateinit var en : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +28,14 @@ class MainActivity : AppCompatActivity() {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        GlideApp.with(this)
+            .load(R.drawable.myphoto)
+            .circleCrop()
+            .override(800, 600)
+            .into(binding.en)
+
 
         binding.img.setOnClickListener({
             if (flag){
@@ -41,9 +55,7 @@ class MainActivity : AppCompatActivity() {
                         binding.mysv.surfaceHolder.unlockCanvasAndPost(canvas)
                     }
                 }
-
             }
         })
-
     }
 }
